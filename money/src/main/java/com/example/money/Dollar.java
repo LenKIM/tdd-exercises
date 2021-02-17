1d59ee4b0f3b1a0139ae9cc8059dbfef4a41de36
lenkim <joenggyu0@gmail.com>
2021-02-17 14:37:34 +0900

    _2_private

--- money/src/main/java/com/example/money/Dollar.java
+++ money/src/main/java/com/example/money/Dollar.java
@@ -2,13 +2,32 @@ package com.example.money;
 
 public class Dollar {
 
-    int amount;
+    private int amount;
 
     public Dollar(int amount) {
         this.amount = amount;
     }
 
-    public void times(int factor) {
-        this.amount *= factor;
+    public Dollar times(int factor) {
+        return new Dollar(this.amount * factor);
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Dollar)) return false;
+
+        Dollar dollar = (Dollar) o;
+
+        return amount == dollar.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return amount;
     }
 }
--- money/src/test/java/com/example/money/DollarTest.java
+++ money/src/test/java/com/example/money/DollarTest.java
@@ -10,7 +10,9 @@ public class DollarTest {
     @Test
     void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
+
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
+
     }
 }
