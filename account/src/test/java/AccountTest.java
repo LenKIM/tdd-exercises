2545b29d4aa05255d5bb5989ba8d081eca09683f
lenkim <joenggyu0@gmail.com>
2021-03-10 14:54:52 +0900

    MODIFY: account project

--- account/src/test/java/AccountTest.java
+++ account/src/test/java/AccountTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class AccountTest {
+    
     private Account account;
 
     public AccountTest() {
@@ -9,24 +10,24 @@ public class AccountTest {
 
     @Test
     void account_create() throws Exception {
-        this.account = new Account(10000);
-        Assertions.assertNotNull(this.account);
+        account = new Account(10000);
+        Assertions.assertNotNull(account);
     }
 
     @Test
     void get_balance() throws Exception {
-        this.account = new Account(10000);
-        if (this.account.getBalance() != 10000) {
+        account = new Account(10000);
+        if (account.getBalance() != 10000) {
             Assertions.fail("Error");
         }
 
-        this.account = new Account(1000);
-        if (this.account.getBalance() != 1000) {
+        account = new Account(1000);
+        if (account.getBalance() != 1000) {
             Assertions.fail("Error");
         }
 
-        this.account = new Account(0);
-        if (this.account.getBalance() != 0) {
+        account = new Account(0);
+        if (account.getBalance() != 0) {
             Assertions.fail("Error");
         }
 
@@ -34,15 +35,15 @@ public class AccountTest {
 
     @Test
     void deposit() {
-        this.account = new Account(10000);
-        this.account.deposit(1000);
-        Assertions.assertEquals(11000, this.account.getBalance());
+        account = new Account(10000);
+        account.deposit(1000);
+        Assertions.assertEquals(11000, account.getBalance());
     }
 
     @Test
     void withdraw() {
-        this.account = new Account(10000);
-        this.account.withdraw(1000);
-        Assertions.assertEquals(9000, this.account.getBalance());
+        account = new Account(10000);
+        account.withdraw(1000);
+        Assertions.assertEquals(9000, account.getBalance());
     }
 }
