70101cc115b96cb8b7672e8bf38f61180c76ecc3
lenkim <joenggyu0@gmail.com>
2021-03-19 16:40:28 +0900

    complete singleModeGame

--- reality-tdd/console-app-model/src/main/java/numberguessing/console/AppModel.java
+++ reality-tdd/console-app-model/src/main/java/numberguessing/console/AppModel.java
@@ -1,21 +1,75 @@
 package numberguessing.console;
 
-import java.util.Arrays;
-
 import numberguessing.PositiveIntegerGenerator;
 
 public final class AppModel {
+
+    private final static String NEW_LINE = System.lineSeparator();
+
+    private String output = "1: Single player game" + NEW_LINE + "2: Multiplayer game" + NEW_LINE + "3: Exit" + NEW_LINE + "Enter selection: ";
+    private static final String SELECT_MODE = "1: Single player game" + NEW_LINE + "2: Multiplayer game" + NEW_LINE + "3: Exit";
+
+    private int answer;
+
+    private boolean completed = false;
+    private final PositiveIntegerGenerator generator;
+
+    Processor processor;
+
+    interface Processor {
+        Processor run(String input);
+    }
+
     public AppModel(PositiveIntegerGenerator generator) {
+        this.generator = generator;
+        processor = new Processor() {
+            @Override
+            public Processor run(String input) {
+                return processModeSelection(input);
+            }
+        };
     }
 
     public boolean isCompleted() {
-        return true;
+
+        return completed;
+    }
+
+
+    private Processor processModeSelection(String input) {
+        if (input.equals("1")) {
+            output = "Single player game" + NEW_LINE + "I'm thinking of a number between 1 and 100."
+                    + NEW_LINE + "Enter your guess: ";
+            answer = generator.generateLessThanOrEqualToHundread();
+            return getSinglePlayerGameProcessor(answer,1);
+        } else {
+            completed = true;
+            return null;
+        }
+    }
+
+    private Processor getSinglePlayerGameProcessor(int answer, int tries) {
+        return input -> {
+            int guess = Integer.parseInt(input);
+            if (guess < answer) {
+                output = "Your guess is too low." + NEW_LINE + "Enter your guess: ";
+                return getSinglePlayerGameProcessor(answer, tries + 1);
+            } else if (guess > answer) {
+                output = "Your guess is too high." + NEW_LINE + "Enter your guess: ";
+                return getSinglePlayerGameProcessor(answer, tries + 1);
+            } else {
+                output = "Correct! " + tries + (tries == 1 ? " guess." : " guesses.") + NEW_LINE + SELECT_MODE
+                        + NEW_LINE + "Enter selection: ";
+                return this::processModeSelection;
+            }
+        };
     }
 
     public String flushOutput() {
-        return null;
+        return output;
     }
 
     public void processInput(String input) {
+        processor = processor.run(input);
     }
 }
--- reality-tdd/console-app-model/src/test/java/numberguessing/console/app/AppModel_specs.java
+++ reality-tdd/console-app-model/src/test/java/numberguessing/console/app/AppModel_specs.java
@@ -3,8 +3,15 @@ package numberguessing.console.app;
 import numberguessing.PositiveIntegerGeneratorStub;
 import numberguessing.console.AppModel;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AppModel_specs {
 
@@ -16,146 +23,146 @@ public class AppModel_specs {
         boolean actual = sut.isCompleted();
         assertFalse(actual);
     }
-//
-//    @Test
-//    void sut_correctly_prints_select_mode_message() {
-//        AppModel sut = new AppModel(new PositiveIntegerGeneratorStub(50));
-//        String actual = sut.flushOutput();
-//
-//        assertThat(actual).isEqualTo("1: Single player game" + NEW_LINE + "2: Multiplayer game" + NEW_LINE + "3: Exit"
-//                + NEW_LINE + "Enter selection: ");
-//    }
-//
-//    @Test
-//    void sut_correctly_exits() {
-//        AppModel sut = new AppModel(new PositiveIntegerGeneratorStub(50));
-//
-//        sut.processInput("3");
-//
-//        boolean actual = sut.isCompleted();
-//        assertTrue(actual);
-//    }
-//
-//    @Test
-//    void sut_correctly_prints_single_player_game_start_message() {
-//        AppModel sut = new AppModel(new PositiveIntegerGeneratorStub(50));
-//        sut.flushOutput();
-//        sut.processInput("1");
-//
-//        String actual = sut.flushOutput();
-//
-//        assertThat(actual).isEqualTo("Single player game" + NEW_LINE + "I'm thinking of a number between 1 and 100."
-//                + NEW_LINE + "Enter your guess: ");
-//    }
-//
-//    @ParameterizedTest
-//    @CsvSource({ "50, 40", "30, 29", "89, 9" })
-//    void sut_correctly_prints_too_low_message_in_single_player_game(int answer, int guess) {
-//        AppModel sut = new AppModel(new PositiveIntegerGeneratorStub(answer));
-//        sut.processInput("1");
-//        sut.flushOutput();
-//        sut.processInput(Integer.toString(guess));
-//
-//        String actual = sut.flushOutput();
-//
-//        assertThat(actual).isEqualTo("Your guess is too low." + NEW_LINE + "Enter your guess: ");
-//    }
-//
-//    @ParameterizedTest
-//    @CsvSource({ "50, 60", "80, 81" })
-//    void sut_correctly_prints_too_high_message_in_single_player_game(int answer, int guess) {
-//        AppModel sut = new AppModel(new PositiveIntegerGeneratorStub(answer));
-//        sut.processInput("1");
-//        sut.flushOutput();
-//        sut.processInput(Integer.toString(guess));
-//
-//        String actual = sut.flushOutput();
-//
-//        assertThat(actual).isEqualTo("Your guess is too high." + NEW_LINE + "Enter your guess: ");
-//    }
-//
-//    @ParameterizedTest
-//    @ValueSource(ints = { 1, 3, 10, 100 })
-//    void sut_correctly_prints_correct_message_in_single_player_game(int answer) {
-//        AppModel sut = new AppModel(new PositiveIntegerGeneratorStub(answer));
-//        sut.processInput("1");
-//        sut.flushOutput();
-//        int guess = answer;
-//        sut.processInput(Integer.toString(guess));
-//
-//        String actual = sut.flushOutput();
-//
-//        assertThat(actual).startsWith("Correct! ");
-//    }
-//
-//    @ParameterizedTest
-//    @ValueSource(ints = { 1, 10, 100 })
-//    void sut_correctly_prints_guess_count_if_single_player_game_finished(int fails) {
-//        AppModel sut = new AppModel(new PositiveIntegerGeneratorStub(50));
-//        sut.processInput("1");
-//        for (int i = 0; i < fails; i++)
-//            sut.processInput("30");
-//        sut.flushOutput();
-//        sut.processInput("50");
-//
-//        String actual = sut.flushOutput();
-//
-//        assertThat(actual).contains((fails + 1) + " guesses." + NEW_LINE);
-//    }
-//
-//    @Test
-//    void sut_correctly_prints_one_guess_if_single_player_game_finished() {
-//        AppModel sut = new AppModel(new PositiveIntegerGeneratorStub(50));
-//        sut.processInput("1");
-//        sut.flushOutput();
-//        sut.processInput("50");
-//
-//        String actual = sut.flushOutput();
-//
-//        assertThat(actual).contains("1 guess.");
-//    }
-//
-//    @Test
-//    void sut_prints_select_mode_message_if_single_player_game_finished() {
-//        AppModel sut = new AppModel(new PositiveIntegerGeneratorStub(50));
-//        sut.processInput("1");
-//        sut.flushOutput();
-//        sut.processInput("50");
-//
-//        String actual = sut.flushOutput();
-//
-//        assertThat(actual).endsWith("1: Single player game" + NEW_LINE + "2: Multiplayer game" + NEW_LINE + "3: Exit"
-//                + NEW_LINE + "Enter selection: ");
-//    }
-//
-//    @Test
-//    void sut_returns_to_mode_selection_if_single_player_game_finished() {
-//        AppModel sut = new AppModel(new PositiveIntegerGeneratorStub(50));
-//
-//        sut.processInput("1");
-//        sut.processInput("50");
-//        sut.processInput("3");
-//
-//        boolean actual = sut.isCompleted();
-//        assertTrue(actual);
-//    }
-//
-//    @ParameterizedTest
-//    @ValueSource(strings = "1, 10, 100")
-//    void sut_generates_answer_for_each_game(String source) {
-//        int[] answers = Stream.of(source.split(",")).map(String::trim).mapToInt(Integer::parseInt).toArray();
-//        AppModel sut = new AppModel(new PositiveIntegerGeneratorStub(answers));
-//        for (int answer : answers) {
-//            sut.processInput("1");
-//            sut.flushOutput();
-//            sut.processInput(Integer.toString(answer));
-//        }
-//
-//        String actual = sut.flushOutput();
-//
-//        assertThat(actual).startsWith("Correct! ");
-//    }
-//
+
+    @Test
+    void sut_correctly_prints_select_mode_message() {
+        AppModel sut = new AppModel(new PositiveIntegerGeneratorStub(50));
+        String actual = sut.flushOutput();
+
+        assertThat(actual).isEqualTo("1: Single player game" + NEW_LINE + "2: Multiplayer game" + NEW_LINE + "3: Exit" + NEW_LINE + "Enter selection: ");
+    }
+
+    @Test
+    void sut_correctly_exits() {
+        AppModel sut = new AppModel(new PositiveIntegerGeneratorStub(50));
+
+        sut.processInput("3");
+
+        boolean actual = sut.isCompleted();
+        assertTrue(actual);
+    }
+
+    @Test
+    void sut_correctly_prints_single_player_game_start_message() {
+        AppModel sut = new AppModel(new PositiveIntegerGeneratorStub(50));
+        sut.flushOutput();
+        sut.processInput("1");
+
+        String actual = sut.flushOutput();
+
+        assertThat(actual).isEqualTo("Single player game" + NEW_LINE + "I'm thinking of a number between 1 and 100."
+                + NEW_LINE + "Enter your guess: ");
+    }
+
+    @ParameterizedTest
+    @CsvSource({"50, 40", "30, 29", "89, 9"})
+    void sut_correctly_prints_too_low_message_in_single_player_game(int answer, int guess) {
+        AppModel sut = new AppModel(new PositiveIntegerGeneratorStub(answer));
+        sut.processInput("1");
+        sut.flushOutput();
+        sut.processInput(Integer.toString(guess));
+
+        String actual = sut.flushOutput();
+
+        assertThat(actual).isEqualTo("Your guess is too low." + NEW_LINE + "Enter your guess: ");
+    }
+
+    @ParameterizedTest
+    @CsvSource({"50, 60", "80, 81"})
+    void sut_correctly_prints_too_high_message_in_single_player_game(int answer, int guess) {
+        AppModel sut = new AppModel(new PositiveIntegerGeneratorStub(answer));
+        sut.processInput("1");
+        sut.flushOutput();
+        sut.processInput(Integer.toString(guess));
+
+        String actual = sut.flushOutput();
+
+        assertThat(actual).isEqualTo("Your guess is too high." + NEW_LINE + "Enter your guess: ");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 3, 10, 100})
+    void sut_correctly_prints_correct_message_in_single_player_game(int answer) {
+        AppModel sut = new AppModel(new PositiveIntegerGeneratorStub(answer));
+        sut.processInput("1");
+        sut.flushOutput();
+        int guess = answer;
+        sut.processInput(Integer.toString(guess));
+
+        String actual = sut.flushOutput();
+
+        assertThat(actual).startsWith("Correct! ");
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 10, 100})
+    void sut_correctly_prints_guess_count_if_single_player_game_finished(int fails) {
+        AppModel sut = new AppModel(new PositiveIntegerGeneratorStub(50));
+        sut.processInput("1");
+        for (int i = 0; i < fails; i++)
+            sut.processInput("30");
+        sut.flushOutput();
+        sut.processInput("50");
+
+        String actual = sut.flushOutput();
+
+        assertThat(actual).contains((fails + 1) + " guesses." + NEW_LINE);
+    }
+
+    @Test
+    void sut_correctly_prints_one_guess_if_single_player_game_finished() {
+        AppModel sut = new AppModel(new PositiveIntegerGeneratorStub(50));
+        sut.processInput("1");
+        sut.flushOutput();
+        sut.processInput("50");
+
+        String actual = sut.flushOutput();
+
+        assertThat(actual).contains("1 guess.");
+    }
+
+    @Test
+    void sut_prints_select_mode_message_if_single_player_game_finished() {
+        AppModel sut = new AppModel(new PositiveIntegerGeneratorStub(50));
+        sut.processInput("1");
+        sut.flushOutput();
+        sut.processInput("50");
+
+        String actual = sut.flushOutput();
+
+        assertThat(actual).endsWith("1: Single player game" + NEW_LINE + "2: Multiplayer game" + NEW_LINE + "3: Exit"
+                + NEW_LINE + "Enter selection: ");
+    }
+
+    @Test
+    void sut_returns_to_mode_selection_if_single_player_game_finished() {
+        AppModel sut = new AppModel(new PositiveIntegerGeneratorStub(50));
+
+        sut.processInput("1");
+        sut.processInput("50");
+        sut.processInput("3");
+
+        boolean actual = sut.isCompleted();
+        assertTrue(actual);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = "100, 10, 1")
+    void sut_generates_answer_for_each_game(String source) {
+        int[] answers = Stream.of(source.split(",")).map(String::trim).mapToInt(Integer::parseInt).toArray();
+        AppModel sut = new AppModel(new PositiveIntegerGeneratorStub(answers));
+        for (int answer : answers) {
+            sut.processInput("1");
+            sut.flushOutput();
+            sut.processInput(Integer.toString(answer));
+        }
+
+        String actual = sut.flushOutput();
+
+        assertThat(actual).startsWith("Correct! ");
+    }
+
 //    @Test
 //    void sut_correctly_prints_multiplayer_game_setup_message() {
 //        AppModel sut = new AppModel(new PositiveIntegerGeneratorStub(50));
@@ -166,7 +173,8 @@ public class AppModel_specs {
 //
 //        assertThat(actual).isEqualTo("Multiplayer game" + NEW_LINE + "Enter player names separated with commas: ");
 //    }
-//
+
+
 //    @Test
 //    void sut_correctly_prints_multiplayer_game_start_message() {
 //        AppModel sut = new AppModel(new PositiveIntegerGeneratorStub(50));
